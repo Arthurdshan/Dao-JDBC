47274657b3fb95396878fe4d31e940f43d4e5ab7
Arthur Han <arthurhan269@gmail.com>
2021-03-29 15:43:11 -0300

    DepartmentDao created

---
+++ src/application/Main2.java
@@ -0,0 +1,19 @@
+package application;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Main2 {
+	
+	public static void main(String[] args) {
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		Department department = departmentDao.findById(1);
+		System.out.println(department);
+		department = departmentDao.findById(2);
+		System.out.println(department);
+		
+		
+	}
+}
