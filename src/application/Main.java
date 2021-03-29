93b3fd64634449ecc2d7ac3aa559427acbafc83b
Arthur Han <arthurhan269@gmail.com>
2021-03-29 14:07:09 -0300

    Insert & updated implemented

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -32,6 +33,17 @@ public class Main {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST 4: seller insert =====");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		SellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: seller update =====");
+		seller = SellerDao.findById(1);
+		seller.setName("Martha Waine");
+		SellerDao.update(seller);
+		System.out.println("Update completed");
 		
 	}
 
