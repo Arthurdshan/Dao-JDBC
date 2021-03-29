b747aaebacf044aaeca2214d06dcf4339a28155b
Arthur Han <arthurhan269@gmail.com>
2021-03-29 01:37:18 -0300

    find by ID implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,24 +1,18 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Department obj = new Department(1, "Books");
-		System.out.println(obj);
-		Seller seller = new Seller(1, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-		
-		SellerDao SellerDao = DaoFactory.createSellerDao();
 
+		SellerDao SellerDao = DaoFactory.createSellerDao();
+		Seller seller = SellerDao.findById(3);
 		System.out.println(seller);
-		
+
 	}
 
 }
